884cd0089fa94c5b465700b22e4225831d3a2c43
Erwin <erwin.philip68@gmail.com>
2024-09-20 11:02:17 +0200

    Todo List

--- js/style.js
+++ js/style.js
@@ -1,60 +1,52 @@
-// fonction ajout
+/* Fonction d'Ajout */
 const addBtn = document.querySelector('#btn');
 const taskCard = document.querySelector(".todoCard");
 const delBtn = document.querySelector('.delBtn');
 const tasksContainer = document.querySelector("#todoCards");
+// getElementById : Renvoie un objet élement représentant l'élément dont la propriété id correspond à la chaîne de caractères spécifiée.
+var targetDiv = document.getElementById('displayCount'); 
+var childDivs = tasksContainer.querySelectorAll("div");
+var content = childDivs.length
+// innerHtml : Permet de remplacer aisément le contenu existant d'un élément par un nouveau contenu.
+targetDiv.innerHTML = content
 
+/* Ajout d'un écouteur dévenement sur le bouton pour appeler une fonction ajout */
+addBtn.addEventListener('click', addTask); 
 
-const childDivs = tasksContainer.querySelectorAll("div");
-
-
-// childDivs.length =1;
-
-
-/* Ajout d'un écouteur dévenement sur le bouton pour
-appeler une fonction ajout */
-addBtn.addEventListener('click', addTask); // add EvenrListener agit en ajoutant une fonction ou un objet.
-
-//Ajout d'évenement sur le bouton pour appeler une fonction supression
-
+/* Ajout d'évenement sur le bouton pour appeler une fonction supression */
 delBtn.addEventListener('click', function(){
     deleteTask(taskCard);
 }); 
 
-
-// Créer Fonction
+/* Création Fonction */
+// add : Ajout d'élément'.
 function addTask(){
-    const newTask = taskCard.cloneNode(true) //copie du noeud sur lequel elle a été appelée
-    const newDelBtn = newTask.querySelector('.delBtn')
+    // cloneNode : Copie du noeud sur lequel elle a été appelée.
+    const newTask = taskCard.cloneNode(true) 
+    // querySelector : Retourne le premier élément dans le document correspondant au sélecteur.
+    const newDelBtn = newTask.querySelector('.delBtn') 
     const newTextArea = newTask.querySelector('.task')
-    
-    
-    var targetDiv = document.getElementById('displayCount');
-    
-    // var content = document.createTextNode();   
-        // const newCardNumberbis = document.querySelectorAll('.cardNumber')
-      
-childDivs.length++;
-console.log(childDivs.length)
-var content = document.getElementById()
+    // querySelectorAll : Renvoie une nodelist statique représentant une liste des éléments du document qui correspondent au groupe de sélecteurs spécifiés.
+    childDivs = tasksContainer.querySelectorAll("div");
+    content = childDivs.length +1
+    targetDiv.innerHTML = content
 
         newTextArea.value ="New Task"
+        // addEventListener : Agit en ajoutant une fonction ou un objet.
         newDelBtn.addEventListener('click', function(){
             deleteTask (newTask);
-        });
-
-
-
-        tasksContainer.appendChild(newTask) //ajout d'un noeud à la fin de la liste des enfants d'un noeud parent
-       
-        
-        
-    
+        })
+        // appenChild : Ajout d'un noeud à la fin de la liste des enfants d'un noeud parent.
+        tasksContainer.appendChild(newTask)  
     }
 
-    
+    // delete : Suppression d'élément.
     function deleteTask(task) {
         task.remove();
+
+        childDivs = tasksContainer.querySelectorAll("div");
+        content = childDivs.length
+        targetDiv.innerHTML = content
     }
 
 
